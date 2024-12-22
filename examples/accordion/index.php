82c7a253f5659c5e9f565079f596c757cf78bc7b
Tomasz Pielczyk <tomasz.pielczyk@gmail.com>
2024-12-22 19:59:18 +0100

    up

--- examples/accordion/index.php
+++ examples/accordion/index.php
@@ -1,11 +1,11 @@
 <?php require '../../val.php' ?>
 
 <div accordion>
-    <div accordion-item>
+    <div>
         <div onclick="val.set(this.up('[accordion]'), {open: '1'})">1</div>
         <div val="if" val-key="open" val-value="1">Content1</div>
     </div>
-    <div accordion-item>
+    <div>
         <div onclick="val.set(this.up('[accordion]'), {open: '2'})">2</div>
         <div val="if" val-key="open" val-value="2">Content2</div>
     </div>
